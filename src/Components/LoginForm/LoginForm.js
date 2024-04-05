3860964ff6dddc0fbe17e68b73855ed6e2cdd0f1
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-04-05 12:09:14 +0200

    Create LoginForm.js

---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <div className="wrapper">
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box"></div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;

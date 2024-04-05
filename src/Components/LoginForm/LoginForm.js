cddfbcb6240548987b00bdee719e843acb821997
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-04-05 12:11:48 +0200

    Update LoginForm.js

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -5,7 +5,12 @@ const LoginForm = () => {
     <div className="wrapper">
       <form action="">
         <h1>Login</h1>
-        <div className="input-box"></div>
+        <div className="input-box">
+          <input type="text" placeholder="Username" required />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required />
+        </div>
       </form>
     </div>
   );
